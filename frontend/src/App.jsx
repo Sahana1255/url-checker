cb79d287112fc70e334ffbb31a10657057d287d2
Madhumitha-stack <madhumitham893@gmail.com>
2025-10-11 10:41:09 +0530

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext";
-import { ScanProvider } from "./context/ScanContext";
-import Navbar from "./components/Navbar";
-import Scanner from "./pages/Scanner";
-import Statistics from "./pages/Statistics";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import { ScanProvider } from './context/ScanContext';
+import Navbar from './components/Navbar';
+import Scanner from './pages/Scanner';
+import Statistics from './pages/Statistics';
 
 function App() {
   return (
     <ThemeProvider>
       <ScanProvider>
-        <BrowserRouter>
-          <div className="min-h-screen bg-gray-50 text-gray-900 dark:bg-gray-950 dark:text-gray-100">
+        <Router>
+          <div className="min-h-screen bg-white dark:bg-black transition-colors">
             <Navbar />
-            <main className="mx-auto max-w-6xl px-4 py-6">
-              <Routes>
-                <Route path="/" element={<Scanner />} />
-                <Route path="/statistics" element={<Statistics />} />
-              </Routes>
-            </main>
+            <Routes>
+              <Route path="/" element={<Scanner />} />
+              <Route path="/statistics" element={<Statistics />} />
+            </Routes>
           </div>
-        </BrowserRouter>
+        </Router>
       </ScanProvider>
     </ThemeProvider>
   );
 }
+
 export default App;
